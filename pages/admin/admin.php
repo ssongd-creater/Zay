49c2e174285f47a1c9fc2ccf549de6f687bf9384
ssongd-creater <sksk1440@gmail.com>
2021-08-24 18:31:16 +0900

    8/24 admin site 구성중

--- pages/admin/admin.php
+++ pages/admin/admin.php
@@ -82,10 +82,49 @@
               </div>
             </div>
 
-            <button type="submit">상품입력</button>
+            <button type="submit" class="pro_insert_btn">상품입력</button>
           </form>
         </div>
-        <div class="mem_admin"></div>
+        <div class="mem_admin">
+          <ul class="mem_admin_lists">
+
+            <li class="mem_admin_tit">
+              <span>선택</span>
+              <span>번호</span>
+              <span>아이디</span>
+              <span>이름</span>
+              <span>레벨</span>
+              <span>수정</span>
+            </li>
+
+            <form action="#" class="mem_admin_form" method="post" id="mem_admin_form">
+              <?php
+              include $_SERVER["DOCUMENT_ROOT"]."/connect/db_conn.php";
+              $sql = "SELECT * FROM zay_mem ORDER BY ZAY_mem_idx LIMIT 5";
+
+              $mem_result = mysqli_query($dbConn,$sql);
+              while($mem_row = mysqli_fetch_array($mem_result)){
+                $mem_idx = $mem_row['ZAY_mem_idx'];
+                $mem_id = $mem_row['ZAY_mem_id'];
+                $mem_name = $mem_row['ZAY_mem_name'];
+                $mem_level = $mem_row['ZAY_mem_level'];
+            ?>
+
+              <li class="mem_admin_con">
+                <span><input type="checkbox" name="item[]" value="<?=$mem_idx?>"></span>
+                <span class="idx"><?=$mem_idx?></span>
+                <span><?=$mem_id?></span>
+                <span><?=$mem_name?></span>
+                <span class="level"><input type="text" value="<?=$mem_level?>" name="mem_level"></span>
+                <span><button type="button" class="update_btn">수정</button></span>
+              </li>
+              <?php } ?>
+            </form>
+          </ul>
+          <div class="select_del_btn">
+            <button type="button">선택삭제</button>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -98,7 +137,24 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="/zay/js/jq.main.js"></script>
   <script src="/zay/js/jq.pro_upload.js"></script>
+  <script>
+  $(function() {
+    $(".update_btn").click(function() {
+      const mem_idx = $(this).parent().siblings('.idx').text();
+      const mem_level = $(this).parent().siblings('.level').find('input').val();
+      //alert(mem_level);
 
+
+      // location.href = `/zay/php/member_update.php?update_idx=${mem_idx}&mem_level=${mem_level}`;
+    });
+
+
+    $(".select_del_btn button").click(function() {
+      $("#mem_admin_form").attr('action',
+        `/zay/php/admin_delete.php`).submit();
+    });
+  });
+  </script>
 
 </body>
 
